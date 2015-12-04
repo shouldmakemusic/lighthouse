315242053c15d4b20c66aa7317a9e18f4f1195db
shouldmakemusic <manuel@hirschauer.net>
2015-12-04 23:27:26 +0100

    added echo for pwd

--- resources/wii2ocs/start_wii2osc.sh
+++ resources/wii2ocs/start_wii2osc.sh
@@ -1,2 +1,8 @@
+#!/bin/bash
+echo "Script executed from: ${PWD}"
+
+BASEDIR=$(dirname $0)
+echo "Script location: ${BASEDIR}"
+cd ${BASEDIR}
 export DYLD_LIBRARY_PATH=.:$DYLD_LIBRARY_PATH
 ./wii2osc
